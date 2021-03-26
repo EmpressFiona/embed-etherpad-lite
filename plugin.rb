c9df5a22ad80f12bea4ce32d93d5cf8ef98d4d50
Queen Fiona <fortemaster@gmail.com>
2021-03-25 20:08:17 -0400

    Testing syntax for raw HTML embed
    This should dump Etherpad exports, without styling information, onto the page as-is. Using for a proof of concept.

--- plugin.rb
+++ plugin.rb
@@ -1,16 +1,18 @@
 # name: embed-etherpad-lite
-# about: Embed Etherpad Lite pads using Onebox.
-# version: 1.0
-# authors: Bernhard Fürst
+# about: Embed Etherpad Lite pads using Onebox; this version uses raw html
+# version: 1.1
+# authors: Bernhard Fürst, Fiona Marie
 # url: https://github.com/fuerst/embed-etherpad-lite
 
 class Onebox::Engine::EtherpadLiteOnebox
   include Onebox::Engine
-  # Try to guess Etherpad Lite links like http(s)://domain.tld:port/p/pad-id
-  matches_regexp /\/p\/[^\/]+$/
+  include Onebox::Engine::LayoutSupport
+  include Onebox::Engine::HTML
 
-  # See https://github.com/ether/etherpad-lite/wiki/Embed-Parameters
+  always_https
+  matches_regexp(/^http?:\/\/empress\.ml/)
+
   def to_html
-    "<iframe name='embed_readwrite' src='#{@url}?showControls=true&showChat=true&showLineNumbers=true&useMonospaceFont=false' style='border-width:0' frameborder='0' scrolling='no' width='100%' height=600></iframe><br><a href='#{@url}' target='_blank'>Goto this EtherPad</a>"
+    "<div>#{raw}</div>"
   end
 end
